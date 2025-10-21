8a6f1cede71f17bdf81a5e0d413ad35ac72eab74
nickbair <bairinc@gmail.com>
2025-10-20 18:09:32 -0600

    Return 200

--- lib/main.ex
+++ lib/main.ex
@@ -12,8 +12,9 @@ defmodule Server do
     # Since the tester restarts your program quite often, setting SO_REUSEADDR
     # ensures that we don't run into 'Address already in use' errors
     {:ok, socket} = :gen_tcp.listen(4221, [:binary, active: false, reuseaddr: true])
-    {:ok, _client} = :gen_tcp.accept(socket)
-    :ok = :gen_tcp.send(socket, "HTTP/1.1 200 OK\r\n\r\n")
+    {:ok, client} = :gen_tcp.accept(socket)
+    :ok = :gen_tcp.send(client, "HTTP/1.1 200 OK\r\n\r\n")
+    :gen_tcp.close(client)
   end
 end
 
